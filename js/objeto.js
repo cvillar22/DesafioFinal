6b161316b5c4f442d24a0766118321b41eff7560
cvillar22 <csorrochevillar@gmail.com>
2021-01-07 19:25:06 -0300

    agregando JS al proyecto

---
+++ js/objeto.js
@@ -0,0 +1,26 @@
+/* PROYECTO: Beonnet, es un sitio web en donde se ofrecen SERVICIOS DIGITALES para poder 
+tener tu página web y de esta manera mostrar todo lo que quieras compartir con el mundo cibernético.
+En la página de Servicios, se muestran los distintos planes que ofrece el equipo de Beonnet para armar 
+un sitio web.
+
+ALCANCE:Establecer como objeto de mi proyecto los planes, y así implementar el carrito de compras,
+ y poder realizar operaciones comerciales exitosas.
+*/
+
+// VARIABLES
+
+
+
+
+//OBJETO
+class Plan {
+  // nombre = '';
+  // precio = '';
+  constructor(nombre, precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+  
+  get Valor(){
+    return "Valor";
+  }
